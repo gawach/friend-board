7ae34b252e7ec030df3dd0cbeda777e34eea45da
gawach <gawa9.1998@gmail.com>
2020-07-13 14:25:04 +0000

    create boards_table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_13_104338) do
+ActiveRecord::Schema.define(version: 2020_07_13_120915) do
+
+  create_table "boards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "target_rank"
+    t.integer "recruit_number"
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "games", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
