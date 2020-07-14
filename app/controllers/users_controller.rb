d5f72bb7b5fb06f1618964ef9a1fc0e32a30c0f6
gawach <gawa9.1998@gmail.com>
2020-07-14 13:01:47 +0000

    Add login signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,34 @@
 class UsersController < ApplicationController
-  def show
+  
+  
+  def index
+    @users = User.order(id: :desc).page(params[:page]).per(25)
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    
+    if @user.save
+      flash[:success] = 'ユーザーの登録に成功しました'
+      redirect_to @user
+    else
+      flash.now[:danger] = 'ユーザーの登録に失敗しました'
+      render :new
+    end
+  end
+  
+  private
+    #Strong Parameter
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
     resources :boards, only: [:index, :show, :new, :create, :destroy]
   end
   
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :create]
   
