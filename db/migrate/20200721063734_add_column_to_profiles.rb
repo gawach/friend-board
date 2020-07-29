6d467100fe449abdf2f497199c5c946a42eb12ec
gawach <gawa9.1998@gmail.com>
2020-07-29 09:01:40 +0000

    Modify migration

--- db/migrate/20200721063734_add_column_to_profiles.rb
+++ db/migrate/20200721063734_add_column_to_profiles.rb
@@ -2,6 +2,5 @@ class AddColumnToProfiles < ActiveRecord::Migration[5.2]
   def change
     add_column :profiles, :game_title, :string
     add_column :profiles, :favorite_character, :string
-    
   end
 end
--- db/migrate/20200721081953_add_column_favorite_character_to_profiles.rb
+++
@@ -1,5 +0,0 @@
-class AddColumnFavoriteCharacterToProfiles < ActiveRecord::Migration[5.2]
-  def change
-    add_column :profiles, :favorite_character, :string
-  end
-end
